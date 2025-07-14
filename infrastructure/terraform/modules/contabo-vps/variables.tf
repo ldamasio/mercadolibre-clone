bf30ffa22d187387b7e5567f4445582d6e68511f
Leandro Damasio <ldamasio@gmail.com>
2025-07-14 03:47:04 -0300

    refactor: update Contabo VPS module variables

--- infrastructure/terraform/modules/contabo-vps/variables.tf
+++ infrastructure/terraform/modules/contabo-vps/variables.tf
@@ -1,1 +1,35 @@
- 
+variable "vps_instances" {
+  description = "Map of VPS instances to create"
+  type = map(object({
+    name        = string
+    location    = string
+    vps_type    = string
+    os_type     = string
+    ssh_key     = string
+    role        = string
+  }))
+  default = {}
+}
+
+variable "contabo_client_id" {
+  description = "Contabo API Client ID"
+  type        = string
+  sensitive   = true
+}
+
+variable "contabo_client_secret" {
+  description = "Contabo API Client Secret"
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_public_key" {
+  description = "SSH public key for VPS access"
+  type        = string
+}
+
+variable "tags" {
+  description = "Tags to apply to resources"
+  type        = map(string)
+  default     = {}
+}
